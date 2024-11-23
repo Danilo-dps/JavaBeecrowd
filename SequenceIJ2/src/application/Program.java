1080739d5c8cf5e606042fe7d03181a8843a7666
Danilo-dps <danilodpsnilo@outlook.com>
2024-11-23 13:31:59 -0300

    Fixed Sequence IJ2

--- SequenceIJ2/src/application/Program.java
+++ SequenceIJ2/src/application/Program.java
@@ -1,22 +1,15 @@
 package application;
 
-import java.util.Scanner;
-
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new  Scanner(System.in);
-		int n = sc.nextInt();
-		int j = 7;
-		
-		for (int i = 1; i < n; i++) {
-			if (i % 2 != 0) {
-				System.out.printf("I=%d J=%d%n", i, j);
-			}
-			j--;
-		}
-		
-		sc.close();
-	}
+        // Loop para a variável I, variando de 1 a 9 com passo de 2
+        for (int i = 1; i <= 9; i += 2) {
+            // Loop para a variável J, começando em 7 e decrementando até 5
+            for (int j = 7; j >= 5; j--) {
+                System.out.printf("I=%d J=%d%n", i, j);
+            }
+        }
+    }
 }
