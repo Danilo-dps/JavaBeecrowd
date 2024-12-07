2da936ec72e8f2bc7a104af41ae1dfb7d95a7edf
Danilo-dps <danilodpsnilo@outlook.com>
2024-12-06 23:00:42 -0300

    Sequence IJ 4

--- SequenceIJ4/src/Main.java
+++ SequenceIJ4/src/Main.java
@@ -1,12 +1,14 @@
-public class App {
+ 
+public class Main {
     public static void main(String[] args) {
-        // Loop para a variável I, variando de 0 a 2 com incremento de 0.2
         for (double i = 0; i <= 2; i += 0.2) {
-            // Loop para a variável J, começando de (i + 1) até (i + 3)
-            for (double j = 1; j <= 3; j++) {
-                System.out.printf("I=%.1f J=%.1f%n", i, i + j);
+            for (int j = 1; j <= 3; j++) {
+                if (i == 0 || i == 1 || i > 1.9) {
+                    System.out.printf("I=%.0f J=%.0f%n", i, j + i);
+                } else {
+                    System.out.printf("I=%.1f J=%.1f%n", i, j + i);
+                }
             }
         }
     }
 }
-
